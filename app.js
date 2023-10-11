d17dd814ca2f5a7a901ba5c89c8a89f6f9b2937e
ashikgligich <ashton.gligich@gmail.com>
2023-10-11 09:24:29 -0400

    e
    e

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const planes = [{name: 'fighter', numbers:[4,15,16,18,22,35] },
-    {name: 'bomber', numbers:[1,2,18,24,25,26,29,52]}]
+const planes = [
+  { name: "fighter", numbers: [4, 15, 16, 18, 22, 35] },
+  { name: "bomber", numbers: [1, 2, 18, 24, 25, 26, 29, 52] },
+];
 
-    array.forEach(planes => {  console.log = numbers
-        
-    });
+planes.forEach((number) => console.log(planes));
+
+planes.forEach((number) => console.log(number));
+
+
+
+
+
+planes.forEach(async (numbers) => {
+   console.log(numbers);
+  });
+
