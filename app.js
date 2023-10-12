c8b612d8d96495a7a7ac9473904c7cca5d16b425
ashikgligich <ashton.gligich@gmail.com>
2023-10-12 09:20:48 -0400

    a
    a

--- app.js
+++ app.js
@@ -3,15 +3,28 @@ const planes = [
   { name: "bomber", numbers: [1, 2, 18, 24, 25, 26, 29, 52] },
 ];
 
-planes.forEach((number) => console.log(planes));
+const set1 = [planes.forEach((planes) => (planes.numbers))];
 
-planes.forEach((number) => console.log(number));
+planes.forEach((planes) => console.log(planes.numbers));
+
+planes.forEach((planes) => console.log(planes.numbers));
 
 
-
+//for (let i = 0; i < planes.length; i++) {
+//  console.log(planes.numbers[i].join(' '));
+//}
 
 
 planes.forEach(async (numbers) => {
    console.log(numbers);
   });
 
+
+
+//planes.filter((fighter) => fighter.slice(fighter.numbers) => console.log (numbers))
+
+const array = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
